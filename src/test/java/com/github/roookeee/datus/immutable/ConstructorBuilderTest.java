4b415fd32a474888aee8d21caadab1a91727e925
roookeee <rooke@posteo.org>
2019-06-04 18:35:15 +0200

    Add PitTest and improve the unit tests by its reports

--- src/test/java/com/github/roookeee/datus/immutable/ConstructorBuilderTest.java
+++ src/test/java/com/github/roookeee/datus/immutable/ConstructorBuilderTest.java
@@ -16,27 +16,43 @@ public class ConstructorBuilderTest {
         List<String> target = new ArrayList<>();
 
         ConstructorBuilder<String, String> constructorBuilder = new ConstructorBuilder<String, String>(s -> s)
-                .spy((in, out) -> target.add(in));
+                .spy((in, out) -> {
+                    target.add(in);
+                    target.add(out);
+                });
         Mapper<String, String> mapper = constructorBuilder.build();
 
         //when
         String result = mapper.convert("Hello world!");
 
         //then
+        assertThat(target).hasSize(2);
         assertThat(target.get(0)).isEqualTo("Hello world!");
+        assertThat(target.get(1)).isEqualTo("Hello world!");
+
+        assertThat(result).isEqualTo("Hello world!");
     }
 
     @Test
     public void postProcessingShouldWorkCorrectly() {
         //given
+        List<String> target = new ArrayList<>();
         ConstructorBuilder<String, String> constructorBuilder = new ConstructorBuilder<String, String>(s -> s)
-                .process((in, out) -> "process result");
+                .process((in, out) -> {
+                    target.add(in);
+                    target.add(out);
+                    return "process result";
+                });
         Mapper<String, String> mapper = constructorBuilder.build();
 
         //when
         String result = mapper.convert("Hello world!");
 
         //then
+        assertThat(target).hasSize(2);
+        assertThat(target.get(0)).isEqualTo("Hello world!");
+        assertThat(target.get(1)).isEqualTo("Hello world!");
+
         assertThat(result).isEqualTo("process result");
     }
 
--- src/test/java/com/github/roookeee/datus/mutable/MutableMappingBuilderTest.java
+++ src/test/java/com/github/roookeee/datus/mutable/MutableMappingBuilderTest.java
@@ -4,6 +4,7 @@ import com.github.roookeee.datus.api.Mapper;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.function.Function;
 
@@ -15,23 +16,65 @@ public class MutableMappingBuilderTest {
     public void shouldAddSpyCorrectly() {
         //given
         List<String> spyList = new ArrayList<>();
-        Mapper<String, String> mapper = new MutableMappingBuilder<String, String>(String::new)
-                .spy((in, out) -> spyList.add(in))
-                .from(Function.identity()).to((out, val) -> val.toUpperCase())
-                .spy((in, out) -> spyList.add(out))
+        Mapper<String, String> uppercaseMapper = new MutableMappingBuilder<String, String>(String::new)
+                .spy((in, out) -> {
+                    spyList.add(in);
+                    spyList.add(out);
+                })
+                .from(Function.identity()).to((out, val) ->val.toUpperCase())
+                .spy((in, out) -> {
+                    spyList.add(in);
+                    spyList.add(out);
+                })
                 .build();
 
         //when
-        mapper.convert("input");
+        String result = uppercaseMapper.convert("input");
 
         //then
-        assertThat(spyList).hasSize(2);
+        assertThat(result).isNotNull();
+        assertThat(spyList).hasSize(4);
         assertThat(spyList.get(0))
-                .withFailMessage("Spy should have been called with the initial value")
+                .withFailMessage("Spy should have been called with the input value")
                 .isEqualTo("input");
         assertThat(spyList.get(1))
-                .withFailMessage("Spy should have been called with the transformed  value")
+                .withFailMessage("Spy should have been called with the initial output value")
+                .isEqualTo("");
+
+        assertThat(spyList.get(2))
+                .withFailMessage("Spy should have been called with the input value")
+                .isEqualTo("input");
+        assertThat(spyList.get(3))
+                .withFailMessage("Spy should have been called with the transformed output value")
                 .isEqualTo("INPUT");
     }
 
+
+    @Test
+    public void shouldProcessCorrectly() {
+        //given
+        List<String> spyList = new ArrayList<>();
+        Mapper<String, String> prefixMapper = new MutableMappingBuilder<String, String>(() -> "prefix-")
+                .from(Function.identity()).to((out,in) -> out+in)
+                .process((in, out) -> {
+                    spyList.add(in);
+                    spyList.add(out);
+                    return out+":processed";
+                })
+                .build();
+
+        //when
+        String result = prefixMapper.convert("input");
+
+        //then
+        assertThat(result).isEqualTo("prefix-input:processed");
+        assertThat(spyList).hasSize(2);
+
+        assertThat(spyList.get(0))
+                .withFailMessage("Process should have been called with the input value")
+                .isEqualTo("input");
+        assertThat(spyList.get(1))
+                .withFailMessage("Process should have been called with the transformed output value")
+                .isEqualTo("prefix-input");
+    }
 }
